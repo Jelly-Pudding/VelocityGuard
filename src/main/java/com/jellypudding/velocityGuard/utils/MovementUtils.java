d4e02cd3afe922ac95ba025f0961ebe0873f45ec
Jelly-Pudding <samwalters11111@hotmail.com>
2025-04-14 14:44:07 +0100

    refactoring

--- src/main/java/com/jellypudding/velocityGuard/utils/MovementUtils.java
+++ src/main/java/com/jellypudding/velocityGuard/utils/MovementUtils.java
@@ -4,6 +4,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -53,4 +54,44 @@ public class MovementUtils {
                blockBelow.getType() == Material.LAVA;
     }
 
+    /**
+     * Get the maximum allowed speed for a player with all applicable modifiers
+     * @param player The player to check
+     * @param baseSpeed The base speed from config
+     * @param elytraLandingTime When the player landed from elytra flight (null if not applicable)
+     * @param currentTime The current time in milliseconds
+     * @return The maximum allowed speed with all modifiers applied
+     */
+    public static double getMaxHorizontalSpeed(Player player, double baseSpeed, Long elytraLandingTime, long currentTime) {
+        double maxSpeed = baseSpeed;
+
+        if (player.hasPotionEffect(PotionEffectType.SPEED)) {
+            int level = player.getPotionEffect(PotionEffectType.SPEED).getAmplifier() + 1;
+            maxSpeed += (level * 0.2 * baseSpeed);
+        }
+
+        if (player.hasPotionEffect(PotionEffectType.SLOWNESS)) {
+            int level = player.getPotionEffect(PotionEffectType.SLOWNESS).getAmplifier() + 1;
+            maxSpeed -= (level * 0.15 * baseSpeed);
+            if (maxSpeed < 0.1) maxSpeed = 0.1;
+        }
+
+        if (player.isSprinting()) {
+            maxSpeed *= 1.3;
+        }
+
+        if (isInLiquid(player)) {
+            maxSpeed *= 0.8;
+        }
+
+        if (player.isGliding()) {
+            maxSpeed *= 5.0;
+        }
+
+        if (elytraLandingTime != null && (currentTime - elytraLandingTime < 1500)) {
+            maxSpeed *= 2.9;
+        }
+
+        return maxSpeed * 1.2;
+    }
 } 
